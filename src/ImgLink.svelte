395b8d74305f284844482ea9edf0d4e18770f2e2
Brad <bradleyglaurie@gmail.com>
2022-05-16 19:12:54 +0100

    Updated ImgLink Added size option Added padding

--- src/ImgLink.svelte
+++ src/ImgLink.svelte
@@ -6,12 +6,12 @@
 	// https://www.npmjs.com/package/@types/pixi.js
 	// To make a floating heart animation when the button is clicked
 
-	export let href, src, alt;
+	export let href, src, alt, size;
 </script>
 
-<a draggable="false" {href} target="_blank" bind:this={btn}
-	><img {src} {alt} /></a
->
+<a draggable="false" {href} target="_blank" bind:this={btn}>
+	<img {src} {alt} width={size} height={size} />
+</a>
 
 <style>
 	a,
@@ -21,5 +21,6 @@
 
 	img {
 		pointer-events: none;
+		padding-inline: 0.5rem;
 	}
 </style>
