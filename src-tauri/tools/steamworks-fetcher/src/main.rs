14441799974b04a45aca809c91927b8dc2687c22
brad <contact@yobson.xyz>
2026-07-07 11:37:38 +0100

    remove steamworks API binaries & add tool to download
    it will automatically grab the version used for our currently used
    version of steamworks-rs as determined from our own Cargo.lock file, the
    version to be used in the bundle is then symlinked to the one from the
    latest tag we are using

---
+++ src-tauri/tools/steamworks-fetcher/src/main.rs
@@ -0,0 +1,101 @@
+use serde::Deserialize;
+#[cfg(unix)]
+use std::os::unix::fs::symlink;
+#[cfg(windows)]
+use std::os::windows::fs::symlink_file;
+use std::{
+    fs, io,
+    path::{Path, PathBuf},
+};
+
+#[derive(Deserialize)]
+struct CargoLock {
+    package: Vec<Package>,
+}
+
+#[derive(Deserialize)]
+struct Package {
+    name: String,
+    version: String,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let root = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../..");
+
+    let lock_path = root.join("Cargo.lock");
+    let lock = fs::read_to_string(lock_path)?;
+    let lock: CargoLock = toml::from_str(&lock)?;
+
+    let version = lock
+        .package
+        .iter()
+        .find(|p| p.name == "steamworks")
+        .map(|p| p.version.as_str())
+        .expect("steamworks not found in Cargo.lock");
+
+    println!("steamworks version: {version}");
+
+    let steam_dir = root.join("steam_api");
+    let version_dir = steam_dir.join(version);
+
+    fs::create_dir_all(&version_dir)?;
+
+    download_if_missing(
+        &version_dir.join("steam_api64.dll"),
+        &format!(
+            "https://github.com/Noxime/steamworks-rs/raw/refs/tags/v{version}/steamworks-sys/lib/steam/redistributable_bin/win64/steam_api64.dll"
+        ),
+    )?;
+
+    download_if_missing(
+        &version_dir.join("libsteam_api.so"),
+        &format!(
+            "https://github.com/Noxime/steamworks-rs/raw/refs/tags/v{version}/steamworks-sys/lib/steam/redistributable_bin/linux64/libsteam_api.so"
+        ),
+    )?;
+
+    update_link(
+        &steam_dir.join("steam_api64.dll"),
+        &PathBuf::from(version).join("steam_api64.dll"),
+    )?;
+
+    update_link(
+        &steam_dir.join("libsteam_api.so"),
+        &PathBuf::from(version).join("libsteam_api.so"),
+    )?;
+
+    Ok(())
+}
+
+fn download_if_missing(path: &Path, url: &str) -> Result<(), Box<dyn std::error::Error>> {
+    if path.exists() {
+        println!("exists: {}", path.display());
+        return Ok(());
+    }
+
+    println!("downloading {}", url);
+
+    let response = ureq::get(url).call()?;
+
+    let mut file = fs::File::create(path)?;
+    let mut reader = response.into_body().into_reader();
+
+    io::copy(&mut reader, &mut file)?;
+
+    Ok(())
+}
+
+fn update_link(link: &Path, target: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    if link.exists() || link.symlink_metadata().is_ok() {
+        fs::remove_file(link)?;
+    }
+
+    #[cfg(unix)]
+    symlink(target, link)?;
+    #[cfg(windows)]
+    symlink_file(target, link)?;
+
+    println!("{} -> {}", link.display(), target.display());
+
+    Ok(())
+}
