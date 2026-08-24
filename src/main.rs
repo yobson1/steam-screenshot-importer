9ae88cf8631bebf14b3b2cb726b790ea9e46ea6c
brad <contact@yobson.xyz>
2026-08-24 14:27:38 +0100

    feat: default window size

--- src/main.rs
+++ src/main.rs
@@ -603,7 +603,7 @@ fn main() {
             gpui_component::init(cx);
             preferences::init(cx);
             configure_themes(cx);
-            let bounds = Bounds::centered(None, size(px(1280.0), px(800.0)), cx);
+            let bounds = Bounds::centered(None, size(px(1388.0), px(780.0)), cx);
             cx.open_window(
                 WindowOptions {
                     window_bounds: Some(WindowBounds::Windowed(bounds)),
